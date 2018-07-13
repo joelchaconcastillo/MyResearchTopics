1c2a950d83f35051abe928bcc59f4fd0d51c413d
joelchaconcastillo <joel.chacon@el-insurgente.cimat.mx>
2018-07-13 18:12:33 -0500

    ..

--- Data/SMS-EMOA/DE/parse.sh
+++ Data/SMS-EMOA/DE/parse.sh
@@ -5,14 +5,14 @@ do
 #OBJ_GD3_CROSS_SELECTON_DIFERENTAL_EVALUATIONS_250000_DTLZ6_SEED_20.txt_2obj
 #POF_MOEAD_UF6_RUN25_seed_25_2.dat#
 #OBJ_NSGAII_SBX_POLYNOMIAL_EVALUATIONS_25000000_WFG9_SEED_29_2.txt
-   mv POF/*_${instance}_SEED_${sed}_2.txt POF/${instance}_2_${sed}
+   cat POF/*_${instance}_SEED_${sed}_2.txt | tail -100 > POF/${instance}_2_${sed}
    done
 done
 for instance in WFG1 WFG2 WFG3 WFG4 WFG5 WFG6 WFG7 WFG8 WFG9 DTLZ1 DTLZ2 DTLZ3 DTLZ4 DTLZ5 DTLZ6 DTLZ7 UF8 UF9 UF10
 do
    for sed in {1..35}   
    do
-   mv POF/*_${instance}_SEED_${sed}_3.txt POF/${instance}_3_${sed}
+   cat POF/*_${instance}_SEED_${sed}_3.txt | tail -100 > POF/${instance}_3_${sed}
 #OBJ_GD3_CROSS_SELECTON_DIFERENTAL_EVALUATIONS_250000_WFG9_SEED_34.txt_3obj
    #mv *_${instance}_*seed_${sed}_3.dat ${instance}_3_${sed}
    done
