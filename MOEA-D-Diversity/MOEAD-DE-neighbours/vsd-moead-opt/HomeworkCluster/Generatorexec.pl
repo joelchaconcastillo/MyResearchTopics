eb5b17ecdba99ce6f460ec0846f311c00805c66d
joel.chacon <joel.chacon@cimat.mx>
2018-07-24 23:00:58 -0500

    lap

---
+++ MOEA-D-Diversity/MOEAD-DE-neighbours/vsd-moead-opt/HomeworkCluster/Generatorexec.pl
@@ -0,0 +1,73 @@
+#Con este script se genera la lista de ejecuciones....
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+my $file = "ExecutionFileDiversity";
+my $fout;
+open($fout, '>' ,$file);
+my $Path =  `cd ..; pwd`;#"/home/joel.chacon/Current/MyResearchTopics/MOEA-D-Diversity/MOEAD-DE/vsd-moead-opt";
+chomp $Path;
+
+####Realizar la búsqueda del parámetro D inicial que proporcione mejores resultados
+my $PathAlgorithm = $Path;
+
+my @Instance = ("DTLZ1", "DTLZ2", "DTLZ3", "DTLZ4", "DTLZ5", "DTLZ6", "DTLZ7");
+my $Di = 0.1;
+my $prob = 0.9;
+my $neighboursize = 10;
+my $pops=100;
+my $max_nfes=250000*$pops;
+
+for(my $t=1; $t<=10; $t++)
+{
+   $neighboursize=$t*10;
+   foreach(@Instance)
+   {
+   	my $nvar;
+   	
+   	for(my $nobj = 2; $nobj <=3; $nobj++)
+   	{
+   
+      	   if($_ eq "DTLZ1")
+   	   {
+   	      $nvar=5+$nobj-1;
+   	   }
+   	   elsif($_ eq "DTLZ7")
+   	   {
+   	      $nvar=20+$nobj-1;
+   	   }
+   	   else
+   	   {
+   	      $nvar=10+$nobj-1;
+   	   }
+   	
+   	   for(my $Sed = 1; $Sed <=35; $Sed++) ##Realizar 35 ejecuciones con distintas semilla de cada instancia..
+   	   {
+   	   	print $fout "~$PathAlgorithm/Ejecutable $PathAlgorithm $_ $Sed $nobj $pops $max_nfes $neighboursize $Di $prob $nvar\n";
+   	   }
+   	}
+   }
+}	
+
+
+@Instance = ("WFG1", "WFG2", "WFG3", "WFG4", "WFG5", "WFG6", "WFG7", "WFG8", "WFG9");
+for(my $t=1; $t<=10; $t++)
+{
+   foreach(@Instance)
+   {
+   	my $nvar;
+   	
+   	for(my $nobj = 2; $nobj <=3; $nobj++)
+   	{
+   	   my $k =4;# 2*($nobj-1);
+   	   my $l =20;# 24-$k;
+   	   $nvar=$l+$k;
+   	
+   	   for(my $Sed = 1; $Sed <=35; $Sed++) ##Realizar 35 ejecuciones con distintas semilla de cada instancia..
+   	   {
+   	   	print $fout "~$PathAlgorithm/Ejecutable $PathAlgorithm $_ $Sed $nobj $pops $max_nfes  $neighboursize $Di $prob $l $k\n";
+   	   }
+   	}
+   }
+}
