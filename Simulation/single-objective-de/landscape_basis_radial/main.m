b8612a52bac1f9d35f86a1538be0c05b4a5e48a6
Your Name <you@example.com>
2018-09-17 18:26:44 -0500

    ..

--- Simulation/single-objective-de/landscape_basis_radial/main.m
+++ Simulation/single-objective-de/landscape_basis_radial/main.m
@@ -1,11 +1,11 @@
 %%Replacement Phase
-N =500
-Min = -10;
-Max = 10;
-
+N =100000
+Min = -1;
+Max = 1;
+modas = 2
 %%Initialization
 Population = Min + rand(N, 2)*(Max-Min);
-Di=5
+Di=1.0
 
 x = Min:0.2:Max;
 y = Min:0.2:Max;
@@ -14,7 +14,7 @@ y = linspace(Min,Max);
 
 [X,Y] = meshgrid(x,y);
 %Z = -X.^2 - Y.^2;
-Z = sin(X) + cos(Y);
+Z = sin(X*modas*pi) + cos(Y*modas*pi);
 figure
 s = contour(X,Y,Z,'ShowText','on');
 %hold on
@@ -31,7 +31,7 @@ pause(5)
 count = 0
 
 %Ranks = zeros(500,2)
-F = sin(Population(:,1)) + cos(Population(:,2)); %%Evaluate the population
+F = sin(Population(:,1)*modas*pi) + cos(Population(:,2)*modas*pi); %%Evaluate the population
 %Sort the best individuals...
 [Candidate, I] = sort(F); %%sort by fitness
 Candidate = I;%Population(I,:)
@@ -40,28 +40,19 @@ Candidate = I;%Population(I,:)
 % hold on
 while size(Candidate,1) >= 2  %%Select the best individuals inside the constraints
  %Select the best individual with max fitness
-  Survivors = [Survivors; Candidate(1)];
+   Survivors = [Survivors; Candidate(1)];
   best = Candidate(1)
-  %FSurvivors = F(I(1),:)
   Candidate = removerows(Candidate,'ind',1);
-%  F(1) = []
-  %= removerows(Candidate,'ind',I(1));
-%  Candidate = Candidate(I(2:(size(I,1)-1)),:)
-  %F = F(I(2:(size(I,1))-1),:)
   [IndexDistances, Distances] = knnsearch(Population(Candidate,:), Population(best,:), 'k', size(Population(Candidate,:),1));
-  
+ 
   Penalized = [Penalized ; Candidate(IndexDistances(Distances<=Di),:)];
   Candidate = removerows(Candidate,'ind',IndexDistances(Distances<=Di));
-  % F(IndexDistances(Distances<=Di)) = []
 end
+%Identify attraction bassis...
+knnsearch(Population(Penalized,:), Population(Survivors,:), 'k', size(Population(Survivors,:),1));
+FPenalized = sin(Population(Penalized,1)*modas*pi) + cos(Population(Penalized,2)*modas*pi);
+FSurvivors = sin(Population(Survivors,1)*modas*pi) + cos(Population(Survivors,2)*modas*pi);
+
  hold on
 %Ranks(i) = Niche_i
   s = scatter( Population(Survivors,1), Population(Survivors,2), 90, 'filled');
-   
-
-
-
-
-
- 
- 
